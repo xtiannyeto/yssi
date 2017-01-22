ce6ec208c6c3b057dc5eed8dcdcfe2218f28a95c
xtian7 <xtian7@live.fr>
2017-01-22 14:34:40 +0100

    Search on Nav
     - implement the research on the nav on keyPress

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { Stores } from '../../../both/collections/stores.collection';
@@ -9,7 +9,9 @@ import style from './app.component.scss';
 
 
 import {InjectUser} from "angular2-meteor-accounts-ui";
-import { MaterializeModule } from 'angular2-materialize';
+import { MaterializeModule, MaterializeAction} from 'angular2-materialize';
+
+import { AppComponentService } from './app.component.service';
 
 @Component({
   selector: 'app',
@@ -18,8 +20,25 @@ import { MaterializeModule } from 'angular2-materialize';
 })
 @InjectUser('user')
 export class AppComponent implements OnInit{
+  modalActions = new EventEmitter<string|MaterializeAction>();
+  searchValue : string;
+  searchdelete:boolean = false;
+  
+  constructor(private componentService: AppComponentService){}
 
-  constructor(){}
+  updateData(value: string) {
+
+      if(!(value.length === undefined) && value.length>=3){
+        this.searchdelete = true;
+        this.componentService.updateData(value);
+      }else{
+        if(this.searchdelete == true){
+                this.componentService.updateData("");
+                this.searchdelete=false;
+              }
+      }
+      
+    }
 
   ngOnInit() {
     console.log(MaterializeModule);
