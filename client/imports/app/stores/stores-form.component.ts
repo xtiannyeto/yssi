f436afde0228bb1473f151092586817e20982941
xtian7 <xtian7@live.fr>
2017-01-29 19:30:30 +0100

    Check the form before submit

--- client/imports/app/stores/stores-form.component.ts
+++ client/imports/app/stores/stores-form.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormBuilder, Validators  } from '@angular/forms';
 import { Meteor } from 'meteor/meteor';
 
 import { Stores } from '../../../../both/collections/stores.collection';
-import { Store } from '../../../../both/models/store.model';
+import { Store, YssiLocation } from '../../../../both/models/store.model';
 
  
 import template from './stores-form.component.html';
@@ -23,6 +23,7 @@ export class StoresFormComponent implements OnInit {
   newStorePosition: {lat:number, lng: number} = {lat: 37.4292, lng: -122.1381};
   stores: Store[]=[];
   images: string[] = [];
+  location : YssiLocation;
 
     constructor(
         private formBuilder: FormBuilder
@@ -31,8 +32,8 @@ export class StoresFormComponent implements OnInit {
     ngOnInit() {
         this.addForm = this.formBuilder.group({
         name: ['', Validators.required],
-        description: [],
-        location: ['', Validators.required]
+        description: [''],
+        location: ['']
         });
     }
     addStore(): void {
@@ -40,20 +41,20 @@ export class StoresFormComponent implements OnInit {
       alert('Please log in to add a a new Store');
       return;
     }
-    if (this.addForm.valid) {
+
+    if(!this.checkFormValue()){
+      return;
+    }
+
       Stores.insert({ 
         name : this.addForm.value.name,
         description: this.addForm.value.descritpion,
-        location: {
-          name : this.addForm.value.location,
-          lat: this.newStorePosition.lat,
-          lng: this.newStorePosition.lng
-        },
+        location: this.location,
         images: this.images,
         owner: Meteor.userId() 
       });
+      
       this.addForm.reset();
-    }
   }
 
   isLoggedIn(){
@@ -68,12 +69,35 @@ export class StoresFormComponent implements OnInit {
   }
 
   yourTitle(){
-    if(this.addForm.value.name === undefined || this.addForm.value.name.length == 0){
-        return "YOUR STORE..."
+
+    let title = "YOUR STORE...";
+    if(this.addForm.value.name === undefined || this.addForm.value.name ==  null){
+      return title;
+    } 
+    if(this.addForm.value.name.length == 0){
+        return title;
     }
     return this.addForm.value.name;
   }
   onImage(imageId: string) {
     this.images.push(imageId);
+  }
+  onSelectLocation(location: YssiLocation){
+    this.location = location;
+  }
+  checkFormValue(){
+    if(this.addForm.value === undefined || this.addForm.value ==  null){
+      return false;
+    }
+    if(this.addForm.value.name === undefined || this.addForm.value.name ==  null){
+      return false;
+    }
+    if(this.addForm.value.description === undefined || this.addForm.value.description ==  null){
+      return false;
+    }
+    if(!this.addForm.valid || this.location === undefined){
+      return false;
+    }
+    return true;
   }
 }
