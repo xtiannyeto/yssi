64696f50f4decde82b98bc4f0d0079a0e8ed87e0
xtian7 <xtian7@live.fr>
2017-02-04 18:55:12 +0100

    modify form for update action

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Location } from '@angular/common';
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
@@ -10,63 +10,109 @@ import template from './app.component.html';
 import style from './app.component.scss';
 
 
-import {InjectUser} from "angular2-meteor-accounts-ui";
-import { MaterializeModule, MaterializeAction} from 'angular2-materialize';
+import { InjectUser } from "angular2-meteor-accounts-ui";
+import { MaterializeModule, MaterializeAction } from 'angular2-materialize';
 
 import { AppComponentService } from './app.component.service';
+
+
 
 @Component({
   selector: 'app',
   providers: [Location],
   template,
-  styles : [ style ]
+  styles: [style]
 })
 @InjectUser('user')
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   location: Location;
-  modalActions = new EventEmitter<string|MaterializeAction>();
-  globalActions = new EventEmitter<string|MaterializeAction>();
-  searchValue : string;
-  searchdelete:boolean = false;
-  toastIsLoggedInMessage:string = "You have to be connected to add your store";
-  
-  constructor(private router: Router, private componentService: AppComponentService, location: Location){ this.location = location; }
+  modalActions = new EventEmitter<string | MaterializeAction>();
+  globalActions = new EventEmitter<string | MaterializeAction>();
+  searchValue: string;
+  searchdelete: boolean = false;
+  toastIsLoggedInMessage: string = "You have to be connected to add your store";
+  path: string[];
+
+  constructor(private router: Router, private componentService: AppComponentService, location: Location) { this.location = location; }
 
   updateData(value: string) {
 
-      if(!(value.length === undefined) && value.length>=3){
-        this.searchdelete = true;
-        this.componentService.updateData(value);
-      }else{
-        if(this.searchdelete == true){
-                this.componentService.updateData("");
-                this.searchdelete=false;
-              }
+    if (!(value.length === undefined) && value.length >= 3) {
+      this.searchdelete = true;
+      this.componentService.updateData(value);
+    } else {
+      if (this.searchdelete == true) {
+        this.componentService.updateData("");
+        this.searchdelete = false;
       }
-      
     }
 
-  ngOnInit() {
-    
   }
-  
-  isLoggedIn(){
+
+  ngOnInit() {
+
+  }
+
+  isLoggedIn() {
     if (!Meteor.userId()) {
       return false;
     }
     return true;
   }
 
-  toastIsLoggedIn(url:string){
-    
-    if(!this.isLoggedIn()){
+  toastIsLoggedIn(url: string) {
+
+    if (!this.isLoggedIn()) {
       this.globalActions.emit('toast');
     }
     this.router.navigate([url]);
   }
 
-  isStoreOrHome(){
+  isStoreOrHome() {
     return this.location.isCurrentPathEqualTo('/') || this.location.isCurrentPathEqualTo('/stores');
+  }
+  saveForm() {
+    this.componentService.saveForm();
+  }
+
+  isOnStore() {
+    return this.location.isCurrentPathEqualTo('/store') || this.location.isCurrentPathEqualTo('/stores');
+  }
+
+  onStoreAndIsOwner() {
+    if (!this.isLoggedIn()) {
+      return false;
+    }
+    var path = this.location.path().split("/");
+
+    if (path === undefined) {
+      return false;
+    }
+    var root = path[1];
+    if (root === undefined) {
+      return false;
+    }
+
+    if (!(this.componentService.getOwner().getValue() == Meteor.userId())) {
+      return false;
+    }
+    return root == "store";
+  }
+
+  showAddButton() {
+    return !this.showSaveButton() && !this.showEditButton();
+  }
+
+  showSaveButton() {
+    return this.location.isCurrentPathEqualTo('/add') || this.location.path().split("/")[1] == 'update';
+  }
+
+  showEditButton() {
+    return this.onStoreAndIsOwner();
+  }
+
+  editForm() {
+    this.componentService.editForm();
   }
 
 }
