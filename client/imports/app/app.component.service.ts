acd2338e7200ccc476fef4d5b4e0b425524e3ace
xtian7 <xtian7@live.fr>
2017-02-04 18:46:53 +0100

    Add method on services

--- client/imports/app/app.component.service.ts
+++ client/imports/app/app.component.service.ts
@@ -2,17 +2,37 @@
 import { Component, OnInit,EventEmitter,Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
+import { User } from '../../../both/models/user.model';
+
 @Injectable()
 export class AppComponentService {
     
     private searchValue = new BehaviorSubject("");
+    private ownerValue = new BehaviorSubject("");
+    public onSaveForm : EventEmitter<boolean> = new EventEmitter<boolean>();
+    public onEditForm : EventEmitter<boolean> = new EventEmitter<boolean>();
 
     getData() {
-        console.log(this.searchValue.getValue());
         return this.searchValue;
     }
 
     updateData(data: string) {
         this.searchValue.next(data);
     }
+
+    saveForm(){
+        this.onSaveForm.emit(true);
+    }
+
+    updateOwner(data: string) {
+       this.ownerValue.next(data);
+    }
+    getOwner(){
+        return this.ownerValue;
+    }
+
+    editForm(){
+        this.onEditForm.emit(true);
+    }
+    
 }
