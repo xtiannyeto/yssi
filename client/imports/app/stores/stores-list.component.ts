6e31416f8fe318aae3c75dcd37ee256b5f469ad7
xtian7 <xtian7@live.fr>
2017-02-04 19:52:35 +0100

    Fix broken research and refactor

--- client/imports/app/stores/stores-list.component.ts
+++ client/imports/app/stores/stores-list.component.ts
@@ -1,4 +1,4 @@
-import { Component,EventEmitter, OnInit, OnDestroy, OnChanges } from '@angular/core';
+import { Component, EventEmitter, OnInit, OnDestroy, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
@@ -12,7 +12,7 @@ import { InfiniteScroll } from 'angular2-infinite-scroll';
 
 import { Meteor } from 'meteor/meteor';
 import { AppComponentService } from '../app.component.service';
-import { MaterializeModule, MaterializeAction} from 'angular2-materialize';
+import { MaterializeModule, MaterializeAction } from 'angular2-materialize';
 
 import 'rxjs/add/operator/combineLatest';
 import 'rxjs/add/operator/concat';
@@ -23,88 +23,84 @@ import { Store } from '../../../../both/models/store.model';
 import template from './stores-list.component.html';
 import style from './stores-list.component.scss';
 
- 
+
 interface Pagination {
   limit: number;
   skip: number;
 }
- 
+
 interface Options extends Pagination {
   [key: string]: any
 }
- 
+
 
 @Component({
   selector: 'store-list',
   template,
-  styles : [ style ]
+  styles: [style]
 })
 
 @InjectUser('user')
-export class StoresListComponent  implements OnInit, OnDestroy {
+export class StoresListComponent implements OnInit, OnDestroy {
   stores$: Observable<Store[]>;
   stores: Store[] = [];
   storesForMap: Observable<Store[]>;
   storesSub: Subscription;
   pageSize: BehaviorSubject<number> = new BehaviorSubject<number>(10);
   curPage: BehaviorSubject<number> = new BehaviorSubject<number>(1);
-  globalActions = new EventEmitter<string|MaterializeAction>();
+  globalActions = new EventEmitter<string | MaterializeAction>();
   nameOrder: Subject<number> = new Subject<number>();
   optionsSub: Subscription;
   storesSize: number = 0;
   autorunSub: Subscription;
-  location: Subject<string> = new Subject<string>();
+  searchValue: Subject<string> = new Subject<string>();
   user: Meteor.User;
   isFullMapView: boolean;
-  actionButton:string = "add";
-  lat:number;
-  lng:number;
-  searchValue:string;
-  toastIsLoggedInMessage:string = "You have to be connected to see more";
+  actionButton: string = "add";
+  lat: number;
+  lng: number;
+  toastIsLoggedInMessage: string = "You have to be connected to see more";
   imagesSubs: Subscription;
 
- 
-  constructor(private router: Router, private paginationService: PaginationService, private componentService:AppComponentService) {
-    
+
+  constructor(private router: Router, private paginationService: PaginationService, private componentService: AppComponentService) {
+
   }
 
   ngOnInit() {
     this.imagesSubs = MeteorObservable.subscribe('images').subscribe();
     this.componentService.getData().subscribe(data => {
-        this.searchValue = data;
-        this.search(data);
-      });
+      this.search(data);
+    });
 
     this.optionsSub = Observable.combineLatest(
       this.pageSize,
       this.curPage,
       this.nameOrder,
-      this.location
-    ).subscribe(([pageSize, curPage, nameOrder, location]) => {
+      this.searchValue
+    ).subscribe(([pageSize, curPage, nameOrder, searchValue]) => {
       const options: Options = {
-         limit: pageSize as number,
-         skip: ((curPage as number) - 1) * (pageSize as number),
+        limit: pageSize as number,
+        skip: ((curPage as number) - 1) * (pageSize as number),
         sort: { name: nameOrder as number }
-    };
+      };
 
       this.paginationService.setCurrentPage(this.paginationService.defaultId(), curPage as number);
-      
-      if(this.storesSub){
+
+      if (this.storesSub) {
         this.storesSub.unsubscribe();
       }
-        this.storesSub = MeteorObservable.subscribe('stores', options, location).subscribe(() =>{
-                this.stores$ = Stores.find({}, {
-                  sort: {
-                      key: 1
-                    }
-                }).forEach((data)=>{
-                  this.stores = this.arrayUnique(this.stores.concat(data));
-                });
+      this.storesSub = MeteorObservable.subscribe('stores', options, searchValue).subscribe(() => {
+        this.stores$ = Stores.find({}, {
+          sort: {
+            key: 1
+          }
+        }).forEach((data) => {
+          this.stores = this.arrayUnique(this.stores.concat(data));
+        });
       });
-
-      
     });
-   
+
     this.paginationService.register({
       id: this.paginationService.defaultId(),
       itemsPerPage: 10,
@@ -115,7 +111,7 @@ export class StoresListComponent  implements OnInit, OnDestroy {
     this.pageSize.next(10);
     this.curPage.next(1);
     this.nameOrder.next(1);
-    this.location.next('');
+    this.searchValue.next('');
 
     this.autorunSub = MeteorObservable.autorun().subscribe(() => {
       this.storesSize = Counts.get('numberOfStores');
@@ -137,61 +133,62 @@ export class StoresListComponent  implements OnInit, OnDestroy {
   removeStore(store: Store): void {
     Stores.remove(store._id);
   }
-  isOwner(store : Store){
+  isOwner(store: Store) {
     return this.user && this.user._id === store.owner;
   }
   search(value: string): void {
-    this.curPage.next(1);
-     this.location.next(value);
+    this.stores=[];
+    this.searchValue.next(value);
   }
   onPageChanged(page: number): void {
     this.curPage.next(page);
   }
   changeSortOrder(nameOrder: string): void {
-     this.nameOrder.next(parseInt(nameOrder));
-   }
+    this.nameOrder.next(parseInt(nameOrder));
+  }
 
-    onScrollUp(){
-      console.log("Up");
-      //this.curPage.next(this.curPage.getValue()-1);
-    }
-    onScrollDown(){
-      console.log("down");
-      var nbMaxPage = Math.floor(this.storesSize/this.pageSize.getValue())+1;
-      var nextCurPage = this.curPage.getValue()+1;
+  onScrollUp() {
+    
+  }
 
-      if(nextCurPage <= nbMaxPage){
-        this.curPage.next(nextCurPage);
+  onScrollDown() {
+    console.log("down");
+    var nbMaxPage = Math.floor(this.storesSize / this.pageSize.getValue()) + 1;
+    var nextCurPage = this.curPage.getValue() + 1;
+
+    if (nextCurPage <= nbMaxPage) {
+      this.curPage.next(nextCurPage);
     }
-      
-    }
-    isLoggedIn(){
+
+  }
+  
+  isLoggedIn() {
     if (!Meteor.userId()) {
       return false;
     }
     return true;
-    }
+  }
 
-  toastIsLoggedIn(url:string, parameter:string){
-    if(!this.isLoggedIn()){
+  toastIsLoggedIn(url: string, parameter: string) {
+    if (!this.isLoggedIn()) {
       this.globalActions.emit('toast');
     }
     this.router.navigate([url, parameter]);
   }
 
-  isImages(store:Store){
+  isImages(store: Store) {
     return store.images && store.images.length > 0;
   }
 
   arrayUnique(array) {
     var a = array.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1);
-        }
+    for (var i = 0; i < a.length; ++i) {
+      for (var j = i + 1; j < a.length; ++j) {
+        if (a[i] === a[j])
+          a.splice(j--, 1);
+      }
     }
     return a;
-}
+  }
 
 }
