e788189e10ff32a7ead2ea30c5da5f7da1ee5515
xtian7 <xtian7@live.fr>
2017-02-05 01:28:23 +0100

    Add remove action  on upload image

--- client/imports/app/stores/stores-upload.component.ts
+++ client/imports/app/stores/stores-upload.component.ts
@@ -1,44 +1,44 @@
-import { Component , OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { upload } from '../../../../both/methods/images.methods';
-import {Subject, Subscription, Observable} from "rxjs";
-import {MeteorObservable} from "meteor-rxjs";
+import { Subject, Subscription, Observable } from "rxjs";
+import { MeteorObservable } from "meteor-rxjs";
 
 
-import { MaterializeModule, MaterializeAction} from 'angular2-materialize';
+import { MaterializeModule, MaterializeAction } from 'angular2-materialize';
 
-import {Thumb} from "../../../../both/models/image.model";
-import {Thumbs} from "../../../../both/collections/images.collection";
+import { Thumb } from "../../../../both/models/image.model";
+import { Thumbs } from "../../../../both/collections/images.collection";
 
 import template from './stores-upload.component.html';
 import style from './stores-upload.component.scss';
 
- 
+
 @Component({
   selector: 'stores-upload',
   template,
-  styles: [ style ]
+  styles: [style]
 })
 export class StoresUploadComponent {
-    fileIsOver: boolean = false;
-    uploading: boolean = false;
-    maxInputFiles: number = 3;
-    filesArray: string[] = [];
-    files: Subject<string[]> = new Subject<string[]>();
-    thumbs: Observable<Thumb[]>;
-    thumbsSubscription: Subscription;
-    globalActions = new EventEmitter<string|MaterializeAction>();
-    @Output() onFile: EventEmitter<string> = new EventEmitter<string>();
-  
-  constructor() {}
+  fileIsOver: boolean = false;
+  uploading: boolean = false;
+  maxInputFiles: number = 3;
+  filesArray: string[] = [];
+  files: Subject<string[]> = new Subject<string[]>();
+  thumbs: Observable<Thumb[]>;
+  thumbsSubscription: Subscription;
+  globalActions = new EventEmitter<string | MaterializeAction>();
+  @Output() onFile: EventEmitter<string> = new EventEmitter<string>();
 
-ngOnInit() {
+  constructor() { }
+
+  ngOnInit() {
     this.files.subscribe((filesArray) => {
       MeteorObservable.autorun().subscribe(() => {
         if (this.thumbsSubscription) {
           this.thumbsSubscription.unsubscribe();
           this.thumbsSubscription = undefined;
         }
- 
+
         this.thumbsSubscription = MeteorObservable.subscribe("thumbs", filesArray).subscribe(() => {
           this.thumbs = Thumbs.find({
             originalStore: 'images',
@@ -59,7 +59,7 @@ ngOnInit() {
     upload(file)
       .then((result) => {
         this.uploading = false;
-         this.addFile(result);
+        this.addFile(result);
       })
       .catch((error) => {
         this.uploading = false;
@@ -68,7 +68,7 @@ ngOnInit() {
   }
 
   addFile(file) {
-    if(this.filesArray.length>=this.maxInputFiles){
+    if (this.filesArray.length >= this.maxInputFiles) {
       this.globalActions.emit('toast');
       return;
     }
@@ -77,29 +77,39 @@ ngOnInit() {
     this.onFile.emit(file._id);
   }
 
-  onFileAdd(){
-    let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
-        let target: HTMLInputElement = <HTMLInputElement> eventObj.target;
-        let files: FileList = target.files;
-        this.OnFileAddList(files);
-       
+  onFileAdd() {
+    let eventObj: MSInputMethodContext = <MSInputMethodContext>event;
+    let target: HTMLInputElement = <HTMLInputElement>eventObj.target;
+    let files: FileList = target.files;
+    this.OnFileAddList(files);
+
   }
-  OnFileAddList(files: FileList){
-      if(!(files === undefined) && !(files[0] === undefined)){
-          for (let i=0; i<files.length; i++) {
-              this.onFileDrop(files[i]);
-            }
-          event = null;
-        }
+  OnFileAddList(files: FileList) {
+    if (!(files === undefined) && !(files[0] === undefined)) {
+      for (let i = 0; i < files.length; i++) {
+        this.onFileDrop(files[i]);
+      }
+      event = null;
+    }
   }
   reset() {
     this.filesArray = [];
     this.files.next(this.filesArray);
   }
-  
-  setFileArray(filesArray: string[]){
+
+  setFileArray(filesArray: string[]) {
     this.filesArray = filesArray;
     this.files.next(this.filesArray);
+  }
+
+  removeThumb(thumb: Thumb) {
+    for (var i = this.filesArray.length - 1; i >= 0; i--) {
+      if (this.filesArray[i] === thumb.originalId) {
+        this.filesArray.splice(i, 1);
+        this.files.next(this.filesArray);
+        Thumbs.remove(thumb._id);
+      }
+    }
   }
 
 }
