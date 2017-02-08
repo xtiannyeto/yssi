73d0bd8ce624ef8d47c65e1ff8cd6f2dd46cea14
xtian7 <xtian7@live.fr>
2017-02-08 08:53:15 +0100

    adjust de server to subscribe to the right comments

--- server/imports/publications/comments.ts
+++ server/imports/publications/comments.ts
@@ -1,9 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { Counts } from 'meteor/tmeasday:publish-counts';
 
-import { Comments } from '../../../both/collections/comments.collection';
-  
-  
-  Meteor.publish('comments', function(){
-    return Comments.collection.find({});
-  });
+import { Comments } from '../../../both/collections/comments.collection'; 
+import { Stores } from '../../../both/collections/stores.collection';
+
+
+  Meteor.publish('comments', function (storeId: string) {
+  const store = Stores.findOne(storeId);
+ 
+  if (!store) {
+    throw new Meteor.Error('404', 'No such Store!');
+  }
+ 
+  return Comments.collection.find({store: storeId});
+    
+});
