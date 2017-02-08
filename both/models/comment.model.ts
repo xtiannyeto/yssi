5f29298d9b600c6470dba08473b5b5293e016361
xtian7 <xtian7@live.fr>
2017-02-08 08:52:29 +0100

    Adjust model for comments

--- both/models/comment.model.ts
+++ both/models/comment.model.ts
@@ -1,6 +1,7 @@
 import { CollectionObject } from './collection-object.model';
 
 export interface Comment {
+  store: string;
   user: string;
   main: string;
   note?: number;
--- both/models/store.model.ts
+++ both/models/store.model.ts
@@ -1,12 +1,10 @@
 import { CollectionObject } from './collection-object.model';
-import { Comment } from './comment.model';
 
 export interface Store extends CollectionObject {
   name: string;
   description: string;
   location: YssiLocation;
   activities?: string[];
-  comments?:Comment[];
   images?: string[];
   owner?: string;
 }
