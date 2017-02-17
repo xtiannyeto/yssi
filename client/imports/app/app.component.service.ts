d111dc3398ae0280493ab8c40043f4bdbae4e730
xtian7 <xtian7@live.fr>
2017-02-17 21:39:21 +0100

    Encode lat and lng (Base64 for the Url) and refactor

--- client/imports/app/app.component.service.ts
+++ client/imports/app/app.component.service.ts
@@ -1,38 +1,56 @@
 
-import { Component, OnInit,EventEmitter,Injectable } from '@angular/core';
+import { Component, OnInit, EventEmitter, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
 
 import { User } from '../../../both/models/user.model';
+import { YssiLocation } from '../../../both/models/store.model';
 
 @Injectable()
 export class AppComponentService {
-    
+
     private searchValue = new BehaviorSubject("");
     private ownerValue = new BehaviorSubject("");
-    public onSaveForm : EventEmitter<boolean> = new EventEmitter<boolean>();
-    public onEditForm : EventEmitter<boolean> = new EventEmitter<boolean>();
+    private yssiLocation = new BehaviorSubject<any>(null);
+    private onLocationChange: EventEmitter<YssiLocation> = new EventEmitter<YssiLocation>();
+    public onSaveForm: EventEmitter<boolean> = new EventEmitter<boolean>();
+    public onEditForm: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     getData() {
         return this.searchValue;
     }
 
+    updateLocation(lng: number, lat: number) {
+        this.yssiLocation.next({'lng': lng, 'lat': lat });
+    }
     updateData(data: string) {
         this.searchValue.next(data);
     }
 
-    saveForm(){
+    saveForm() {
         this.onSaveForm.emit(true);
     }
 
     updateOwner(data: string) {
-       this.ownerValue.next(data);
+        this.ownerValue.next(data);
     }
-    getOwner(){
+    getOwner() {
         return this.ownerValue;
     }
 
-    editForm(){
+    editForm() {
         this.onEditForm.emit(true);
     }
-    
+
+    getLocation() {
+        return this.yssiLocation;
+    }
+
+    encodeThis(float: number) {
+        return window.btoa(float.toString());
+    }
+    decodeThis(encodeString:string){
+       return  parseFloat(window.atob(encodeString));
+    }
+
 }
