47557869d4f0de6df5a9f5406f2f15540b875fb9
xtian7 <xtian7@live.fr>
2017-02-17 21:38:22 +0100

    Change the route for store-list for adding lat and long

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -10,7 +10,7 @@ import {HomeComponent} from "./home/home.component";
  
 export const routes: Route[] = [
   { path: '', component: HomeComponent },
-  { path: 'stores', component: StoresListComponent },
+  { path: 'stores/:lng/:lat', component: StoresListComponent },
   { path: 'store/:storeId', component: StoreDetailsComponent , canActivate: ['canActivateForLoggedIn']},// for action on loggin only
   { path: 'update/:storeId', component: StoresFormComponent , canActivate: ['canActivateForLoggedIn']},
   { path: 'add', component: StoresFormComponent, canActivate: ['canActivateForLoggedIn']},
