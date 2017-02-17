bec24572d84f7faaaf91f848b326e65266484a78
xtian7 <xtian7@live.fr>
2017-02-17 21:37:13 +0100

    Refactore the home search by location and return the store List

--- client/imports/app/home/home.component.ts
+++ client/imports/app/home/home.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit,EventEmitter } from '@angular/core';
+import {Component, OnInit, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import {MaterializeAction} from 'angular2-materialize';
+import { MaterializeAction } from 'angular2-materialize';
+import { StoreMapComponentService } from '../shared/services/store-map.component.service';
+
+
+import { YssiLocation } from '../../../../both/models/store.model';
 
 import template from './home.component.html';
-import style from '../../home.component.scss';
+import style from './home.component.scss';
+import { Base64 } from 'js-base64';
 
 @Component({
   selector: 'home',
   template
 })
 export class HomeComponent {
-  modalActions = new EventEmitter<string| MaterializeAction>();
-  openModal() {
-    this.modalActions.emit("openModal");
+
+  private onLocationChange: EventEmitter<YssiLocation> = new EventEmitter<YssiLocation>();
+  constructor(private mapService: StoreMapComponentService) { }
+
+  ngOnInit() {
+    this.mapService.setPlaces('home-search-bar', true);
   }
-  closeModal() {
-    this.modalActions.emit("closeModal");
-  }
+
 }
