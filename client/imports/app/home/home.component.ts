45680bd90c8e53e51e597b46899254fa8bc0a76b
xtian7 <xtian7@live.fr>
2017-02-18 12:31:02 +0100

    Add the form of the home and handle the research exactly as the nav form

--- client/imports/app/home/home.component.ts
+++ client/imports/app/home/home.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { MaterializeAction } from 'angular2-materialize';
+
 import { StoreMapComponentService } from '../shared/services/store-map.component.service';
+import { AppComponentService } from '../app.component.service';
 
 
 import { YssiLocation } from '../../../../both/models/store.model';
@@ -12,15 +15,30 @@ import { Base64 } from 'js-base64';
 
 @Component({
   selector: 'home',
-  template
+  template,
+  styles: [style]
 })
 export class HomeComponent {
+  searchForm: FormGroup;
 
   private onLocationChange: EventEmitter<YssiLocation> = new EventEmitter<YssiLocation>();
-  constructor(private mapService: StoreMapComponentService) { }
+  constructor(
+    private mapService: StoreMapComponentService,
+    private componentService: AppComponentService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
-    this.mapService.setPlaces('home-search-bar', true);
+    this.searchForm = this.formBuilder.group({
+      place: ['', Validators.required],
+      searchText: ['', Validators.required],
+    });
+    this.mapService.setPlaces('home-search-bar', false);
+  }
+  
+  submitSearch() {
+    this.componentService.updateData(this.searchForm.value.searchText);
+    this.mapService.toStores();
   }
 
 }
