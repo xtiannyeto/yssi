1d31f5c2848782d6530960208cb08a19e77dd88e
xtian7 <xtian7@live.fr>
2017-06-28 00:21:43 +0200

    Retrigger travis

--- client/main.ts
+++ client/main.ts
@@ -1,13 +1,12 @@
-import 'angular2-meteor-polyfills';
+import "angular2-meteor-polyfills";
 
-//import "materialize-css";
 import "angular2-materialize";
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './imports/app/app.module';
+import { AppModule } from "./imports/app/app.module";
 
 Meteor.startup(() => {
-    const platform = platformBrowserDynamic();
-    platform.bootstrapModule(AppModule);
+  const platform = platformBrowserDynamic();
+  platform.bootstrapModule(AppModule);
 });
