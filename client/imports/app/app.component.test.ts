66b822f7c75d6a49590de2b2fa01dd19a76a1226
xtian7 <xtian7@live.fr>
2017-12-05 12:44:32 +0100

    Test continus integration

---
+++ client/imports/app/app.component.test.ts
@@ -0,0 +1,11 @@
+// chai uses as asset library
+import { assert } from "chai";
+
+// Angular 2 tests imports
+import { TestBed, TestModuleMetadata } from "@angular/core/testing";
+
+describe("Test", () => {
+  it("should just run", () => {
+    assert.equal(5, 5);
+  });
+});
